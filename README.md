8d335da988c492d51d89214af06cff63a68bedb1
Simeon Nikulin <simeonikulin@gmail.com>
2026-06-03 11:06:58 +0100

    fix: updated README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # easy-qr-code
+
+QRCode.com
+QR Codes were developed by Japanese engineer Masahiro Hara as part of the Denso Wave Corporation.
--- src/QRCodeGenerator.java
+++ src/QRCodeGenerator.java
@@ -1,5 +1,6 @@
 public class QRCodeGenerator {
 
+
     public static boolean storedInArray(char[] array, char find) {
         boolean found = false;
 
@@ -10,13 +11,13 @@ public class QRCodeGenerator {
         }
 
         return found;
+
     }
 
     public static void main() {
 
         int[][] qrCodeSizeArray = {};
         String userInput = "";
-
 
         // flags for variety of symbols in user input
         // 0 - numeric, 1 - alphanumeric, 2 - kanji, 3 - 8-bit byte data
@@ -29,18 +30,20 @@ public class QRCodeGenerator {
         char[] alphaNumericSubset = {' ', '$', '*', '+', '-', '.', '/', ':', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 
 
-        for (int i = 0, inputLength = userInput.length(); i < inputLength; i++ ) { // n is defined so that s.length() is evaluated once
+        for (int i = 0, inputLength = userInput.length(); i < inputLength; i++ ) { // n is defined so that userInput.length() is evaluated once
             char symbol = userInput.charAt(i);
+            int symbolCode = (int) symbol;
 
-            // ASCII and Unicode characters 48 - 57 are 0 - 9
+            // checking for exclusive numeric characters 0 - 9
             if (storedInArray(numericSubset, symbol)) {
                 characterTypes[0] = true;
 
-            // ASCII and Unicode characters 65 - 90 are A - Z
+            // checks for exclusive alphanumeric characters SP $ * + - . / : and capital letters A to Z
             } else if (storedInArray(alphaNumericSubset, symbol)) {
                 characterTypes[1] = true;
 
             }
+
         }
 
     }
